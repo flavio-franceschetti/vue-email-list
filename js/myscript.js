a74fa8f8eda8929ebbef1d369d670202347364b9
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-27 17:35:46 +0200

    fix: mounted error

--- js/myscript.js
+++ js/myscript.js
@@ -3,11 +3,12 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      inLoading: true,
       email: [],
     };
   },
   methods: {},
-  onMounted() {
+  mounted() {
     for (let i = 0; i < 10; i++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
